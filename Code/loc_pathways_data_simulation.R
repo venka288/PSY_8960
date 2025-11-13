0c3cbd3a3f76408e02ca9b5c9721a12d7e199a13
Varsha Venkatesh <venka288@umn.edu>
2025-11-13 12:03:15 -0600

    moving file location and cleaning

--- Code/loc_pathways_data_simulation.R
+++ Code/loc_pathways_data_simulation.R
@@ -1,6 +1,6 @@
 #######################################
 #### Locus of Control Pathways Project
-#### Data Simulation Practice
+#### Data Simulation
 #### Varsha Venkatesh
 #### 2025-11-06
 #######################################
@@ -21,84 +21,6 @@ sessionInfo()
 
 # setting seed
 set.seed(1978)
-
-#### using rnorm()
-
-# raw simulation
-rnorm(n = 10, mean = 3.5, sd = 0.75)
-
-# save as dataframe
-outcome <- rnorm(n = 100000, mean = 3.5, sd = 0.75)
-dat_rnorm <- data.frame(outcome)
-
-mean(dat_rnorm$outcome)
-sd(dat_rnorm$outcome)
-
-# rnorm(), setting up a function
-data_generate <- function(n, m, sd) {
-  set.seed(1978)
-  g <- rnorm(n = n, mean = m, sd = sd)
-  data <- data.frame(
-    outcome = g
-  )
-  return(data)
-}
-
-dat_rnorm_func <- data_generate(n = 10, m = 3.5, sd = 0.75)
-dat_rnorm_func
-mean(dat_rnorm_func$outcome)
-sd(dat_rnorm_func$outcome)
-
-dat_rnorm_func_again <- data_generate(n = 10000, m = 4.2, sd = 0.2)
-dat_rnorm_func_again
-mean(dat_rnorm_func_again$outcome)
-sd(dat_rnorm_func_again$outcome)
-
-# rnorm() with two groups
-data_generate <- function(n1, n2, m1, m2, sd1, sd2) {
-  set.seed(1978)
-  g1 <- rnorm(n = n1, mean = m1, sd = sd1)
-  g2 <- rnorm(n = n2, mean = m2, sd = sd2)
-  data <- data.frame(
-    group = rep(c("1", "2"), times = c(n1, n2)),
-    outcome = c(g1, g2)
-  )
-  return(data)
-}
-
-dat_rnorm_func_two <- data_generate(n1 = 500, n2 = 50,
-                                    m1 = 0.39, m2 = -0.42,
-                                    sd1 = 0.93, sd2 = 0.80)
-
-# checking data
-head(dat_rnorm_func_two)
-
-dat_rnorm_func_two %>% 
-  group_by(group) %>% 
-  summarize(mean = mean(outcome, na.rm = TRUE), sd = sd(outcome, na.rm = TRUE))
-
-#### using rnorm_multi() from faux package
-
-# set parameters
-n <- 100
-m1 <- 0.39
-sd1 <- 0.93
-m2 <- -0.42
-sd2 <- 0.80
-
-# create data file
-dat_faux <- rnorm_multi(n = n,
-                        mu = c(m1, m2),
-                        sd = c(sd1, sd2),
-                        varnames = c("g1", "g2")
-                        )
-
-# checking data
-head(dat_faux)
-mean(dat_faux$g1)
-mean(dat_faux$g2)
-sd(dat_faux$g1)
-sd(dat_faux$g2)
 
 ### using sample() to simulate data
 
